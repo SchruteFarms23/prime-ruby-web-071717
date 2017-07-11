4a284dca43a80dfbbcdd78ba9a7385ad4f3428a3
SchruteFarms23 <romanmullady@gmail.com>
2017-07-11 19:45:26 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+	if num.negative? || num == 0
+		return false
+	end
+  new_arr = []
+  arr= (1..num).to_a
+  arr.each do |int|
+   new_arr << int if num%int == 0
+  end
+  if new_arr.length > 2 || num == 1
+    false
+  else 
+    true
+  end
+end
